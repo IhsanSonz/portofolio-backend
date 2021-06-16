0da8572eedc52c7d16217062697c087b33e9edb6
IhsanKirson <mnurihsamutaqin66.siswa@smkn4bdg.sch.id>
2021-06-16 20:03:02 +0700

    category controller crud

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -32,5 +33,11 @@ Route::middleware(['middleware' => 'jwt.verify'])->group(function () {
     Route::post('product', [ProductController::class, 'store']);
     Route::put('product/{id}', [ProductController::class, 'update']);
     Route::delete('product/{id}', [ProductController::class, 'destroy']);
+
+    Route::get('category', [CategoryController::class, 'index']);
+    Route::get('category/{id}', [CategoryController::class, 'show']);
+    Route::post('category', [CategoryController::class, 'store']);
+    Route::put('category/{id}', [CategoryController::class, 'update']);
+    Route::delete('category/{id}', [CategoryController::class, 'destroy']);
 
 });
